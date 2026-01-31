86be2f9cc25a57d6b374f3ca81fd55d1f4d54de6
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2026-01-31 09:32:30 +0530

    Create navigation_bar.dart with two-page navigation
    Implement a simple Flutter app with navigation between two pages.

---
+++ navigation_bar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FirstPage(),
+    );
+  }
+  
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SecondPage(),
+              ),
+            );
+          },
+          child: Text('Go to next page'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go to first Page'),
+        ),
+      ),
+    );
+  }
+}
