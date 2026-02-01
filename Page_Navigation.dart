4a279829dadb8512eb633b387387b416ab13de63
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2026-02-01 22:10:02 +0530

    Add basic page navigation between FirstPage and SecondPage
    Implement a simple page navigation with two pages using Flutter.

---
+++ Page_Navigation.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SecondPage(),
+              ),
+            );
+          },
+          child: Text('Go to  the next page'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go to the first Page'),
+        ),
+      ),
+    );
+  }
+}
